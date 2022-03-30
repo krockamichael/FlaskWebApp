a521d6a6e1a2d32bc6d43fc1a492ce352daa82ac
krockamichael <krocka.michael@gmail.com>
2022-03-30 12:32:48 +0200

    Prepare implementation of predict method.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask
+import keras
+import numpy as np
+
+MODEL_PATH = "models\keras_mAlexNet\out_keras.h5"
 
 app = Flask(__name__)
+model = keras.models.load_model(MODEL_PATH)
 
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
 
-@app.route("/<int:celsius>")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+def predict(image) -> str:
+    return "occupied" if np.argmax(model.predict(image)) == 1 else "empty"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
