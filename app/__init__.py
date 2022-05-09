4339d599a9c137a1626660d6e5def6ef2e7212b5
krockamichael <krocka.michael@gmail.com>
2022-05-09 11:37:59 +0200

    _

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'krocka.michael@gmail.com'
-    app.config['MAIL_PASSWORD'] = '7bleqf4nnb' # 'dummy_password'
+    app.config['MAIL_PASSWORD'] = 'dummy_password'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
 
